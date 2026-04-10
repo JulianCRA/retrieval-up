7014aaebbfcd7d1aedc944ded18956054aa162dc
juliancra <contact@julianr.dev>
2026-04-10 15:58:33 +0700

    - conversion a 16KHz - conversion a monocanal - seleccion de codec pcm_s16le

--- 01. descargador/src/descargador/__main__.py
+++ 01. descargador/src/descargador/__main__.py
@@ -1,10 +1,28 @@
 import argparse
 import sys
 from pathlib import Path
+from urllib.parse import urlparse
+
 
 AUDIO_EXTS = {".mp3", ".wav", ".flac", ".ogg", ".m4a", ".opus", ".wma", ".aac"}
 VIDEO_EXTS = {".mp4", ".mkv", ".webm", ".avi", ".mov", ".flv", ".wmv"}
 MEDIA_EXTS = AUDIO_EXTS | VIDEO_EXTS
+
+OPTS = {
+        "format": "bestaudio/best",
+        "outtmpl": str(Path("descargas") / "%(title)s - %(id)s.%(ext)s"),
+        "prefer_ffmpeg": True,
+        "noplaylist": False,
+        "geo_bypass": True,
+        "no_warnings": True,
+        "quiet": False,
+        "writethumbnail": False,
+        "enable_file_urls": True,
+        "postprocessors": [
+            {"key": "FFmpegExtractAudio", "preferredcodec": "wav", "preferredquality": "0"}
+        ],
+        "postprocessor_args": ["-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le"]
+    }
 
 def main():
     parser = argparse.ArgumentParser(
@@ -30,6 +48,8 @@ def main():
 
     if args.local:
         procesar_fuente_local(args.local)
+    elif args.remoto:
+        procesar_fuente_remota(args.remoto)
 
 def procesar_fuente_local(ruta):
     path = Path(ruta)
@@ -75,13 +95,34 @@ def leer_directorio(ruta):
     return archivos
 
 def procesar_archivo(ruta):
-    print(f"Cargando archivo: {ruta}")
+    # TODO : revisar que el contenido no exista ya en la carpeta de descargas, o que no se haya descargado antes
+    with YoutubeDL(OPTS) as ydl:
+        info = ydl.extract_info(ruta.as_uri(), download=True)
 
 def procesar_batch(batch):
     if batch:
         print(f"Cargando {len(batch)} archivos del batch:")
         for archivo in batch:
             procesar_archivo(archivo)
+
+
+def validar_url(u: str) -> bool:
+    # puede ser mucho mas estyricta
+    u = u.strip()
+    p = urlparse(u)
+    return p.scheme in ("http", "https") and bool(p.netloc)
+
+from yt_dlp import YoutubeDL
+
+def procesar_fuente_remota(url):
+    if not validar_url(url):
+        print(f"Error: '{url}' no es una URL válida.")
+        sys.exit(1)
+
+    print(f"Descargando desde URL remota: {url}")
+
+    with YoutubeDL(OPTS) as ydl:
+        info = ydl.extract_info(url, download=True)
 
 if __name__ == "__main__":
     main()
